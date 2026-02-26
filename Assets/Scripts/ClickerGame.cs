73cb4f74f1306dfd0e1c87f1087397d1a33bca86
Ethan Voraritskul <contactethanv@gmail.com>
2026-02-26 15:34:50 -0600

    script changes
    messed up gitignore

--- Assets/Scripts/ClickerGame.cs
+++ Assets/Scripts/ClickerGame.cs
@@ -109,7 +109,20 @@ public class ClickerGame : MonoBehaviour
             enemyHPBar.SetActive(enemyHP > 0);
         }
     }
-
+    
+/*Complete (1 to 2) of the following tasks for extra credit:
+   
+   EC Task A: Death sounds [5 points]:
+   A sound that plays when the enemy dies [2.5 points]
+   A different sound plays when the cult dies [2.5 points]
+   EC Task B: Add a button to restart the game within the gameplay scene [5 points]
+   EC Task C: Create and add another enemy [5 points]
+   Add a new sprite and prefab for the second enemy with an animation [2 points]
+   Each time the scene is loaded, a random enemy should be created [3 points]
+   EC Task D: Create a mainMenu scene that has:
+   A background image (Should be done using a canvas to be eligible for full credit)  [+2 points]
+   A button to start the game [+1 points]
+   The game transitions from the mainMenu to gameplay when the button is pressed [2 points]*/
 
     
     
